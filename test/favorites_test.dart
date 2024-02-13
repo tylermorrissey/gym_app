ccd02c91b697c6e4927663d2f5a51b91fbfcc439
tylermorrissey <tylerpmorrissey@gmail.com>
2024-02-13 18:18:37 -0500

    feat: learning some more testing

--- test/favorites_test.dart
+++ test/favorites_test.dart
@@ -1,22 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:gym_app/models/favorites.dart';
-import 'package:test/test.dart';
+import 'package:gym_app/screens/favorites.dart';
+import 'package:provider/provider.dart';
+
+late Favorites favoritesList;
+
+Widget createFavoritesScreen() => ChangeNotifierProvider<Favorites>(
+      create: (context) {
+        favoritesList = Favorites();
+        return favoritesList;
+      },
+      child: const MaterialApp(
+        home: FavoritesPage(),
+      ),
+    );
+
+void addItems() {
+  for (var i = 0; i < 10; i += 2) {
+    favoritesList.add(i);
+  }
+}
 
 void main() {
-  group('Testing App Provider', () {
-    var favorites = Favorites();
-
-    test('An item should be added', () {
-      var number = 35;
-      favorites.add(number);
-      expect(favorites.items.contains(number), true);
+  group('Favorites Page Widget Tests', () {
+    testWidgets('Test if ListView shows up', (tester) async {
+      await tester.pumpWidget(createFavoritesScreen());
+      addItems();
+      await tester.pumpAndSettle();
+      expect(find.byType(ListView), findsOneWidget);
     });
 
-    test('An item should be removed', () {
-      var number = 45;
-      favorites.add(number);
-      expect(favorites.items.contains(number), true);
-      favorites.remove(number);
-      expect(favorites.items.contains(number), false);
+    testWidgets('Testing Remove Button', (tester) async {
+      await tester.pumpWidget(createFavoritesScreen());
+      addItems();
+      await tester.pumpAndSettle();
+      var totalItems = tester.widgetList(find.byIcon(Icons.close)).length;
+      await tester.tap(find.byIcon(Icons.close).first);
+      await tester.pumpAndSettle();
+      expect(tester.widgetList(find.byIcon(Icons.close)).length,
+          lessThan(totalItems));
+      expect(find.text('Removed from favorites.'), findsOneWidget);
     });
   });
 }
--- test/home_test.dart
+++ test/home_test.dart
@@ -24,5 +24,18 @@ void main() {
       await tester.pumpAndSettle();
       expect(find.text('Item 0'), findsNothing);
     });
+
+    testWidgets('Testing IconButtons', (tester) async {
+      await tester.pumpWidget(createHomeScreen());
+      expect(find.byIcon(Icons.favorite), findsNothing);
+      await tester.tap(find.byIcon(Icons.favorite_border).first);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      expect(find.text('Added to favorites.'), findsOneWidget);
+      expect(find.byIcon(Icons.favorite), findsWidgets);
+      await tester.tap(find.byIcon(Icons.favorite).first);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      expect(find.text('Removed from favorites.'), findsOneWidget);
+      expect(find.byIcon(Icons.favorite), findsNothing);
+    });
   });
 }
