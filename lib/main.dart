3748855b270984d392af712d369f0e80be3a31ea
tylermorrissey <tylerpmorrissey@gmail.com>
2024-02-13 17:12:41 -0500

    feat: trying out go_router, learning some testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,241 +1,70 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:english_words/english_words.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:gym_app/lift.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gym_app/screens/favorites.dart';
+import 'package:gym_app/screens/home.dart';
 import 'package:provider/provider.dart';
+
+import 'models/favorites.dart';
+
+//firestore stuff
+// var db = FirebaseFirestore.instance;
+// var data = {'favorites.dart': favorites.dart.map((f) => f.toLowerCase().asString)};
+// db
+//     .collection('favorites.dart')
+//     .doc('current')
+//     .set(data, SetOptions(merge: true));
+
+// updateFavorites();
+// db.collection('favorites.dart').get().then((event) {
+// for (var doc in event.docs) {
+// print('${doc.id} => ${doc.data()}');
+// }
+// });
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseFirestore.instance.settings = const Settings(
-    host: '127.0.0.1:8080',
-    sslEnabled: false,
-    persistenceEnabled: false,
-  );
+  // await Firebase.initializeApp();
+  // FirebaseFirestore.instance.settings = const Settings(
+  //   host: '127.0.0.1:8080',
+  //   sslEnabled: false,
+  //   persistenceEnabled: false,
+  // );
   runApp(MyApp());
 }
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: HomePage.routeName,
+      builder: (context, state) {
+        return const HomePage();
+      },
+      routes: [
+        GoRoute(
+          path: FavoritesPage.routeName,
+          builder: (context, state) {
+            return const FavoritesPage();
+          },
+        ),
+      ],
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'gym App',
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp.router(
+        title: 'Testing Sample',
         theme: ThemeData(
+          primarySwatch: Colors.blue,
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-  var db = FirebaseFirestore.instance;
-
-  void updateFavorites() {
-    // merges existing data
-    var data = {'favorites': favorites.map((f) => f.toLowerCase().asString)};
-    db
-        .collection('favorites')
-        .doc('current')
-        .set(data, SetOptions(merge: true));
-  }
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-
-    updateFavorites();
-    db.collection('favorites').get().then((event) {
-      for (var doc in event.docs) {
-        print('${doc.id} => ${doc.data()}');
-      }
-    });
-    notifyListeners();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
-
-  late FirebaseFirestore firestore;
-  String host = '127.0.0.1';
-  int count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-      case 1:
-        page = FavoritesPage();
-      case 2:
-        page = Lift();
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              child: NavigationRail(
-                extended: constraints.maxWidth >= 600,
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Home'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('Favorites'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.sports_gymnastics),
-                    label: Text('Create lifts'),
-                  ),
-                ],
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (value) {
-                  setState(() {
-                    selectedIndex = value;
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                child: page,
-              ),
-            ),
-          ],
-        ),
-      );
-    });
-  }
-}
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
-    );
-  }
-}
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: '${pair.first} ${pair.second}',
-        ),
+        routerConfig: _router,
       ),
     );
   }
---
+++ test/favorites_test.dart
@@ -0,0 +1,22 @@
+import 'package:gym_app/models/favorites.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Testing App Provider', () {
+    var favorites = Favorites();
+
+    test('An item should be added', () {
+      var number = 35;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+    });
+
+    test('An item should be removed', () {
+      var number = 45;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+      favorites.remove(number);
+      expect(favorites.items.contains(number), false);
+    });
+  });
+}
