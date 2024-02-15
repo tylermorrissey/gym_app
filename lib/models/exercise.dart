718fc833e0c81bc6b87bf59d487f20039bbc6349
tylermorrissey <tylerpmorrissey@gmail.com>
2024-02-15 04:45:07 -0500

    feat: saving exercise data model for later

--- lib/models/exercise.dart
+++ lib/models/exercise.dart
@@ -3,23 +3,23 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'exercise.g.dart';
 
 //TODO Kind of worthless
-@riverpod
-class Exercises extends _$Exercises {
-  @override
-  List<String> build() {
-    final List<String> exercise = [];
-    return exercise;
-  }
-
-  Object list() {
-    return state;
-  }
-
-  void add(String name) {
-    state.add(name);
-  }
-
-  void remove(String name) {
-    state.remove(name);
-  }
+// @riverpod
+// class Exercises extends _$Exercises {
+//   @override
+//   List<String> build() {
+//     final List<String> exercise = [];
+//     return exercise;
+//   }
+//
+//   Object list() {
+//     return state;
+//   }
+//
+//   void add(String name) {
+//     state.add(name);
+//   }
+//
+//   void remove(String name) {
+//     state.remove(name);
+//   }
 }
